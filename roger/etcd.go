e9aac2a1eaba60f46e976e369a08e4b2c52c6b2f
Alex Buchanan <buchanae@ohsu.edu>
2017-10-15 02:51:40 -0700

    etcd

---
+++ roger/etcd.go
@@ -0,0 +1,31 @@
+package roger
+
+import (
+  "github.com/xordataexchange/crypt/config"
+)
+
+type EtcdProvider struct {
+  Servers []string
+  cm config.ConfigManager
+}
+
+func NewEtcdProvider(servers []string) *EtcdProvider {
+  return &EtcdProvider{Servers: servers}
+}
+
+func (c *EtcdProvider) Init() error {
+  cm, err := config.NewStandardEtcdConfigManager(c.Servers)
+  if err != nil {
+    return err
+  }
+  c.cm = cm
+  return nil
+}
+
+func (c *EtcdProvider) Lookup(key string) (interface{}, error) {
+  v, err := c.cm.Get(key)
+	if err != nil {
+		return nil, nil
+	}
+	return v, nil
+}
