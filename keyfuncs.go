5647a4add3cab2565996640ac411a3677b19f07b
Alex Buchanan <buchanae@gmail.com>
2018-12-13 13:24:00 -0800

    more doc cleanup

--- keyfuncs.go
+++ keyfuncs.go
@@ -9,17 +9,17 @@ type KeyFunc func([]string) string
 
 var (
   // DotKey joins key parts with a "." and converts to lowercase.
-  DotKey KeyFunc = func(k []string) string {
-	  return strings.ToLower(strings.Join(k, "."))
-  }
+  DotKey KeyFunc = lowerJoin(".")
 
   // UnderscoreKey joins key parts with a "_" and converts to lowercase.
-  UnderscoreKey = func(k []string) string {
-	  return strings.ToLower(strings.Join(k, "_"))
-  }
+  UnderscoreKey = lowerJoin("_")
+
+  // DashKey joins key parts with a "-" and converts to lowercase.
+  DashKey = lowerJoin("-")
 )
 
-// DashKeyFunc joins key parts with a "-" and converts to lowercase.
-func DashKeyFunc(k []string) string {
-	return strings.ToLower(strings.Join(k, "-"))
+func lowerJoin(delim string) KeyFunc {
+  return func(k []string) string {
+    return strings.ToLower(strings.Join(k, delim))
+  }
 }
